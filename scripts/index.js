30a68c0ab7a10a6d3d845f0076f1e0fe23a59972
Petra Zemanová <zemape42@gmail.com>
2022-02-04 19:17:43 +0100

    Add user preference to local storage

--- scripts/index.js
+++ scripts/index.js
@@ -20,30 +20,48 @@ function iconMode(color) {
 }
 
 function toggleDarkLightMode(isDark) {
-  isDark
+  isDark === DARK_THEME
     ? moonElm.classList.replace('show-icon', 'hide-icon')
     : moonElm.classList.replace('hide-icon', 'show-icon');
-  isDark
+  isDark === DARK_THEME
     ? sunElm.classList.replace('hide-icon', 'show-icon')
     : sunElm.classList.replace('show-icon', 'hide-icon');
-  isDark ? iconMode(DARK_THEME) : iconMode(LIGHT_THEME);
+  isDark === DARK_THEME ? iconMode(DARK_THEME) : iconMode(LIGHT_THEME);
+}
+
+function setTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  window.localStorage.setItem('theme', theme);
+  toggleDarkLightMode(theme);
 }
 
 function changeTheme(event) {
   if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', DARK_THEME);
-    window.localStorage.setItem('theme', DARK_THEME);
-    toggleDarkLightMode(true);
+    setTheme(DARK_THEME);
   } else {
-    document.documentElement.setAttribute('data-theme', LIGHT_THEME);
-    window.localStorage.setItem('theme', LIGHT_THEME);
-    toggleDarkLightMode(false);
+    setTheme(LIGHT_THEME);
   }
 }
 
 checkboxElm.addEventListener('click', changeTheme);
 
-// hamburger menu
+//set dark/light mode according to user preference
+function setThemeOnLoad() {
+  let themePreference = window.localStorage.getItem('theme');
+  switch (themePreference) {
+    case DARK_THEME:
+      checkboxElm.checked = true;
+      setTheme(DARK_THEME);
+      break;
+    case LIGHT_THEME:
+      setTheme(LIGHT_THEME);
+      break;
+  }
+}
+
+setThemeOnLoad();
+
+// hamburger menu toggle
 function toggleMenu(event) {
   if (event.target.id == 'menu-btn') {
     navElm.classList.add('nav-visible');
